876167c7eecea77a4a46136b95b59d613eabbd3d
Ana Lívia <analivia.gdc@gmail.com>
2022-03-18 17:19:49 -0300

    feita função retornaArrayORdenado

--- quinzena-1/aula2/modulo1/revisao-js/exercicios.js
+++ quinzena-1/aula2/modulo1/revisao-js/exercicios.js
@@ -5,49 +5,42 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   return array.length
+    return array.length
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
     let novoArray = []
-  for(i=array.length ; i>0; i--) {
-    novoArray.push(array[i-1])
-  }
-  return novoArray
+    for (i = array.length; i > 0; i--) {
+        novoArray.push(array[i - 1])
+    }
+    return novoArray
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  let i = 0
-  let j = 0
-  let novoArray = []
-  while(i<array.length) {
-    for(j=0 ; j<array.length ; j++) {
-        let itemMenor = array[i]
-        if(itemMenor > array[j]) {
-            itemMenor = array[j]
-        }
-    }
-    novoArray.push(itemMenor)
-    i++
-  }
-
+    return array.sort((a, b) => { return a - b })
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+    return array.filter((valor) => {
+        if (valor % 2 === 0) {
+            return true
+        } else {
+            return false
+        }
+    })
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+
 }
 
 // EXERCÍCIO 07
@@ -57,7 +50,7 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+
 }
 
 // EXERCÍCIO 09
@@ -67,27 +60,27 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+
 }
 
 // EXERCÍCIO 14
@@ -97,10 +90,10 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
+
 }
