d28d2696b0fd497e091aad2f13e3b3f84a145e31
Ana Lívia <analivia.gdc@gmail.com>
2022-05-05 14:17:01 -0300

    Feito projeto Tarefas

---
+++ modulo3/bloco8/ciclo-vida-todo/src/styled.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const AppDad = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const Container = styled.div`
+    flex: 1 1 0%;
+    padding: 20px;
+    border: 1px solid gray;
+    width: 600px;
+    height: 50vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const Input = styled.div`
+    width: 70%;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const Tarefas = styled.div`
+    width: 80%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 5px;
+`
