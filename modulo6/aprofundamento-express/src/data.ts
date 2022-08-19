7dbbf9e68f7fefdce9b90d341649bfb14036713f
Ana Lívia Cruz <analivia.gdc@gmail.com>
2022-08-18 23:45:46 -0300

    feito incompleto

---
+++ modulo6/aprofundamento-express/src/data.ts
@@ -0,0 +1,39 @@
+export type Afazeres = {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
+
+export const afazeres: Afazeres[] = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+      },
+      {
+        "userId": 1,
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": true
+      },
+      {
+        "userId": 2,
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+      },
+      {
+        "userId": 3,
+        "id": 4,
+        "title": "et porro tempora",
+        "completed": true
+      },
+      {
+        "userId": 3,
+        "id": 5,
+        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false
+      }
+]
---
+++ modulo6/aprofundamento-express/src/index.ts
@@ -0,0 +1,104 @@
+import express, {Request, Response} from "express";
+import cors from 'cors'
+import { AddressInfo } from "net";
+import { afazeres, Afazeres } from "./data";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/ping', (req, res) => {
+    res.send('pong! 🏓')
+})
+
+
+app.get('/todos', (req: Request, res: Response) => {
+    const status: string | undefined = req.query.status?.toString()
+    console.log(status)
+    try{
+        if (!status) {
+            throw new Error("Campo obrigatório")
+        } else{
+            res.send(afazeres.filter(afazer => afazer.completed.toString() === status))
+        }
+    } catch(error: any) {
+        res.send(error.message)
+    }
+})
+
+app.post('/create', (req: Request, res: Response) => {
+    const newList: Afazeres = {
+        userId: req.body.userId,
+        id: Date.now(),
+        title: req.body.title,
+        completed: false
+    }
+
+    afazeres.push(newList)
+    res.status(201).send(afazeres)
+})
+
+app.put('/edit/:id', (req: Request, res: Response) => {
+    const id = Number(req.params.id)
+    const tarefa = afazeres.find((tarefa) => tarefa.id === id)
+    try{
+        if (!tarefa) {
+            throw new Error("Tarefa não encontrada!")
+        } else{
+            let status = false
+            const newList: any[] = afazeres.map((task) => {
+                if(task.id === id) {
+                    task.completed = !tarefa.completed
+                    status = task.completed
+                }
+            })
+            res.send(status ? "Sua tarefa foi completada!" : "Sua tarefa ainda está pendente!")
+        }
+    } catch(error: any) {
+        res.send(error.message)
+    }
+})
+
+app.delete('/delete/:id', (req: Request, res: Response) => {
+    const id = Number(req.params.id)
+    const tarefa = afazeres.find((tarefa) => tarefa.id === id)
+    const tarefaIndex = afazeres.findIndex((tarefa) => tarefa.id === id)
+    try{
+        if (!tarefa) {
+            throw new Error("Tarefa não encontrada!")
+        } else{
+            afazeres.splice(tarefaIndex, 1)
+            res.send("Sua tarefa foi excluída!")
+        }
+    } catch(error: any) {
+        res.send(error.message)
+    }
+})
+
+app.get('/todos/:userId', (req: Request, res: Response) => {
+    const status: string | undefined = req.query.status?.toString()
+    console.log(status)
+    try{
+        if (!status) {
+            throw new Error("Campo obrigatório")
+        } else{
+            res.send(afazeres.filter(afazer => afazer.completed.toString() === status))
+        }
+    } catch(error: any) {
+        res.send(error.message)
+    }
+})
+
+
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+});;
