2a9624a2d83748024394e9accd1c39d8ea56b09f
Ana Lívia Cruz <analivia.gdc@gmail.com>
2022-08-23 23:53:26 -0300

    feito questões

---
+++ modulo6/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,28 @@
+export type Produto = {
+    id: string
+    name: string
+    price: number
+}
+
+export const produtos = [
+    {
+        id: "1",
+        name: "Calça jeans",
+        price: 300
+    },
+    {
+        id: "2",
+        name: "Vestido",
+        price: 250
+    },
+    {
+        id: "3",
+        name: "Lingerie",
+        price: 120
+    },
+    {
+        id: "4",
+        name: "Cropped",
+        price: 70
+    }
+]
